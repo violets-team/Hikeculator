c141cb129bf622418cbb61a41dcb1a26a5089bb5
grey- <grey-552@yandex.ru>
2022-02-21 17:50:07 +0300

    add fragment hike

--- app/src/main/java/com/example/hikeculator/HikeFragment.kt
+++ app/src/main/java/com/example/hikeculator/HikeFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.hikeculator.databinding.FragmentHikeBinding
 import com.example.hikeculator.databinding.FragmentMembersBinding
 
@@ -14,10 +16,16 @@ class HikeFragment: Fragment(R.layout.fragment_hike) {
     private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentHikeBinding.inflate(inflater, container, false)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
