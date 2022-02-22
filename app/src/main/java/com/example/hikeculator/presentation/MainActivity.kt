c83104c54a245f8bd621161ea7f54b6393f6899c
grey- <grey-552@yandex.ru>
2022-02-22 22:09:43 +0300

    renamed ui elements

--- app/src/main/java/com/example/hikeculator/presentation/MainActivity.kt
+++ app/src/main/java/com/example/hikeculator/presentation/MainActivity.kt
@@ -1,12 +1,14 @@
-package com.example.hikeculator
+package com.example.hikeculator.presentation
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.hikeculator.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_hike)
+        setContentView(R.layout.activity_main)
     }
 }
---
+++ app/src/main/java/com/example/hikeculator/presentation/create_hike/CreateHikeFragment.kt
@@ -0,0 +1,16 @@
+package com.example.hikeculator.presentation.create_hike
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.example.hikeculator.R
+import com.example.hikeculator.databinding.FragmentCreateHikeBinding
+import com.example.hikeculator.databinding.FragmentTripGeneralBinding
+
+class CreateHikeFragment: Fragment(R.layout.fragment_create_hike) {
+
+    private val binding by viewBinding(FragmentCreateHikeBinding::bind)
+}
