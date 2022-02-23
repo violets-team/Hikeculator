0c89f86f13b436e408addbf263bb55b774bec8a3
NikolayKuts <survivenik@gmail.com>
2022-02-23 11:47:54 +0300

    Add GeneralTripViewModel, TripRepository implementation

---
+++ app/src/main/java/com/example/hikeculator/data/repository_implementation/TripRepositoryImpl.kt
@@ -0,0 +1,19 @@
+package com.example.hikeculator.data.repository_implementation
+
+import com.example.hikeculator.domain.entities.Trip
+import com.example.hikeculator.domain.repositories.TripRepository
+
+class TripRepositoryImpl : TripRepository {
+
+    override fun insertTrip(trip: Trip) {
+        TODO("Not yet implemented")
+    }
+
+    override fun removeTrip() {
+        TODO("Not yet implemented")
+    }
+
+    override fun fetchTrips(): Set<Trip> {
+        TODO("Not yet implemented")
+    }
+}
