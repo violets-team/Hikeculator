3dab46d62bbc86f408f02f126406de64e88a0513
NikolayKuts <survivenik@gmail.com>
2022-02-23 11:50:12 +0300

    Remove гттуÑсуunnecessary imports in MainActivity

--- app/src/main/java/com/example/hikeculator/presentation/MainActivity.kt
+++ app/src/main/java/com/example/hikeculator/presentation/MainActivity.kt
@@ -3,10 +3,6 @@ package com.example.hikeculator.presentation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.hikeculator.R
-import com.example.hikeculator.domain.entities.User
-import com.example.hikeculator.domain.enums.Gender
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
