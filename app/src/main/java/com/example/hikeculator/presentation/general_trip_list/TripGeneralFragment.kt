454d2f122dda7740755e77b6fd997256ca4c9bb0
NikolayKuts <survivenik@gmail.com>
2022-02-28 14:51:12 +0300

    Rename UI elements

--- app/src/main/java/com/example/hikeculator/presentation/general_trip_list/TripGeneralFragment.kt
+++ app/src/main/java/com/example/hikeculator/presentation/general_trip_list/TripGeneralFragment.kt
@@ -6,20 +6,20 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.hikeculator.R
-import com.example.hikeculator.databinding.FragmentGeneralTripBinding
+import com.example.hikeculator.databinding.FragmentGeneralTripsBinding
 
-class TripGeneralFragment: Fragment(R.layout.fragment_general_trip) {
+class TripGeneralFragment: Fragment(R.layout.fragment_general_trips) {
 
-    private val binding by viewBinding(FragmentGeneralTripBinding::bind)
+    private val binding by viewBinding(FragmentGeneralTripsBinding::bind)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.fabCreateHike.setOnClickListener {
+        binding.actionButtonGeneralTripsCreateTrip.setOnClickListener {
            findNavController().navigate(R.id.action_hikesFragment_to_createHikeFragment)
         }
 
-        binding.bottomAppBarHikes.setOnMenuItemClickListener {
+        binding.bottomAppBarGeneralTrips.setOnMenuItemClickListener {
             when(it.itemId){
                 R.id.app_bar_item_profile -> {
                     findNavController().navigate(R.id.action_hikesFragment_to_profileFragment)
