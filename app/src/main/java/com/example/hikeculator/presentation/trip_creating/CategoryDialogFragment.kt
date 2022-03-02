374e8ddaa148b24b90ef0b2632d2e35c81675db4
grey- <grey-552@yandex.ru>
2022-03-03 00:09:16 +0300

    Renaming UI elements

---
+++ app/src/main/java/com/example/hikeculator/presentation/trip_creating/CategoryDialogFragment.kt
@@ -0,0 +1,12 @@
+package com.example.hikeculator.presentation.trip_creating
+
+import androidx.fragment.app.DialogFragment
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.example.hikeculator.R
+import com.example.hikeculator.databinding.DialogFragmentTripCategoryChoosingBinding
+
+class CategoryDialogFragment: DialogFragment(R.layout.dialog_fragment_trip_category_choosing) {
+
+    private val binding by viewBinding(DialogFragmentTripCategoryChoosingBinding::bind)
+
+}
