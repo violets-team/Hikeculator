538c572edd02271407a3df866ae7580ba83ee4cc
Sambitrex <sasha54364@yandex.ru>
2022-03-16 17:40:54 +0300

    Implement food search screen

---
+++ app/src/main/java/com/example/hikeculator/presentation/food_search/FoodSearchFragment.kt
@@ -0,0 +1,6 @@
+package com.example.hikeculator.presentation.food_search
+
+import androidx.fragment.app.Fragment
+import com.example.hikeculator.R
+
+class FoodSearchFragment : Fragment(R.layout.fragment_food_search)
