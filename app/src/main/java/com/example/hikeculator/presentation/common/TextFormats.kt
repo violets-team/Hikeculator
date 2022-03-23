fdb44dee6e5c7b50af7b9cc6f4ce338c4ddebf71
Sambitrex <sasha54364@yandex.ru>
2022-03-23 09:45:00 +0300

    Clean up

--- app/src/main/java/com/example/hikeculator/presentation/common/TextFormats.kt
+++
@@ -1,9 +0,0 @@
-package com.example.hikeculator.presentation.common
-
-const val FORMAT_CALORIES = "%.2f kcal"
-
-const val FORMAT_FAT = "Fat %.2f"
-
-const val FORMAT_CARBS = "Carbs %.2f"
-
-const val FORMAT_PROTEIN = "Protein %.2f"
