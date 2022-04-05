444b8b4308287936eef3edcddfb3dc430c23b91e
Sambitrex <sasha54364@yandex.ru>
2022-04-05 04:13:07 +0300

    Clean up merge conflicts

--- app/src/main/java/com/example/hikeculator/data/fiebase/entities/FirestoreProduct.kt
+++ app/src/main/java/com/example/hikeculator/data/fiebase/entities/FirestoreProduct.kt
@@ -4,6 +4,7 @@ import com.example.hikeculator.data.fiebase.DEFAULT_LONG_VALUE
 import com.example.hikeculator.data.fiebase.DEFAULT_STRING_VALUE
 
 data class FirestoreProduct(
+    val id: String = DEFAULT_STRING_VALUE,
     val name: String = DEFAULT_STRING_VALUE,
     val weight: Long = DEFAULT_LONG_VALUE,
     val nutritionalValue: FirestoreNutritionValue = FirestoreNutritionValue(),
