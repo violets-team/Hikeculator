2b242eee396ad332368c56d3ea837f996b74197d
Sambitrex <sasha54364@yandex.ru>
2022-04-07 02:50:23 +0300

    Add product search auto complete request

---
+++ app/src/main/java/com/example/hikeculator/data/retrofit/ProductSearchAutoCompleteService.kt
@@ -0,0 +1,28 @@
+package com.example.hikeculator.data.retrofit
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ProductSearchAutoCompleteService {
+
+    val apiService: ProductSearchAutoCompleteApi by lazy {
+        buildRetrofit().create(
+            ProductSearchAutoCompleteApi::class.java
+        )
+    }
+
+    private fun buildRetrofit(): Retrofit {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+        val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
+
+        return Retrofit
+            .Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}
--- app/src/main/java/com/example/hikeculator/data/retrofit/ProductSearchService.kt
+++ app/src/main/java/com/example/hikeculator/data/retrofit/ProductSearchService.kt
@@ -1,7 +1,5 @@
 package com.example.hikeculator.data.retrofit
 
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -10,22 +8,9 @@ object ProductSearchService {
     val apiService: ProductSearchApi by lazy { buildRetrofit().create(ProductSearchApi::class.java) }
 
     private fun buildRetrofit(): Retrofit {
-        val loggingInterceptor = getLoggingInterceptor()
-        val client = getOkHttpClient(loggingInterceptor)
-
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
-            .client(client)
             .build()
     }
-
-    private fun getLoggingInterceptor(): HttpLoggingInterceptor {
-        val loggingInterceptor = HttpLoggingInterceptor()
-        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
-        return loggingInterceptor
-    }
-
-    private fun getOkHttpClient(interceptor: HttpLoggingInterceptor) =
-        OkHttpClient.Builder().addInterceptor(interceptor).build()
 }
