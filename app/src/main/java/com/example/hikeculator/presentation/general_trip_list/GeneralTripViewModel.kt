d2893d99c9ec3e139203957ba495d4f7dbdf30bf
NikolayKuts <survivenik@gmail.com>
2022-04-18 22:41:01 +0300

    Refactore

--- app/src/main/java/com/example/hikeculator/presentation/general_trip_list/GeneralTripViewModel.kt
+++ app/src/main/java/com/example/hikeculator/presentation/general_trip_list/GeneralTripViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.hikeculator.presentation.general_trip_list
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.hikeculator.R
