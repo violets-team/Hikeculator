1ad1598430583422b5b6e0c8705a1d0d6eae3ce3
NikolayKuts <survivenik@gmail.com>
2022-04-29 21:57:42 +0300

    Write tests for TripDetailViewModel

--- app/src/main/java/com/example/hikeculator/presentation/trip_details/TripDetailViewModel.kt
+++ app/src/main/java/com/example/hikeculator/presentation/trip_details/TripDetailViewModel.kt
@@ -3,16 +3,11 @@ package com.example.hikeculator.presentation.trip_details
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.hikeculator.R
-import com.example.hikeculator.domain.entities.DayMeal
-import com.example.hikeculator.domain.entities.Product
 import com.example.hikeculator.domain.entities.Trip
 import com.example.hikeculator.domain.entities.TripDay
 import com.example.hikeculator.domain.interactors.TripDayInteractor
 import com.example.hikeculator.domain.interactors.TripInteractor
-import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.flow.*
-import kotlinx.coroutines.launch
-import java.util.*
 
 class TripDetailViewModel(
     private val tripInteractor: TripInteractor,
@@ -20,40 +15,15 @@ class TripDetailViewModel(
     private val tripId: String,
 ) : ViewModel() {
 
+    private val _problemMessage = MutableSharedFlow<Int>(replay = 1, extraBufferCapacity = 1)
+    val problemMessage: SharedFlow<Int> = _problemMessage.asSharedFlow()
+
     val data: SharedFlow<Pair<Trip?, List<TripDay>>> = getTripData().shareIn(
         scope = viewModelScope,
         started = SharingStarted.Eagerly,
         replay = 1
     )
 
-    private val _problemMessage = MutableSharedFlow<Int>()
-    val problemMessage: SharedFlow<Int> = _problemMessage.asSharedFlow()
-
-    fun updateTripDay(
-        tripId: String,
-        breakfastProducts: List<Product>,
-        lunchProducts: List<Product>,
-        dinnerProducts: List<Product>,
-        snackProducts: List<Product>,
-    ) {
-
-        val tripDay = TripDay(
-            id = UUID.randomUUID().toString(),
-            date = System.currentTimeMillis(),
-            breakfast = DayMeal(products = breakfastProducts),
-            lunch = DayMeal(products = lunchProducts),
-            dinner = DayMeal(products = dinnerProducts),
-            snack = DayMeal(products = snackProducts)
-        )
-
-        val exceptionHandler = CoroutineExceptionHandler { _, _ ->
-            _problemMessage.tryEmit(R.string.problem_with_trip_updating)
-        }
-
-        viewModelScope.launch(context = exceptionHandler) {
-            tripDayInteractor.insertTripDay(tripId = tripId, tripDay = tripDay)
-        }
-    }
 
     private fun getTripData(): Flow<Pair<Trip?, List<TripDay>>> {
         return combine(
@@ -70,7 +40,7 @@ class TripDetailViewModel(
 
     private fun getTrip(): Flow<Trip?> {
         return tripInteractor.fetchTrip(tripId = tripId).catch {
-            _problemMessage.tryEmit(R.string.problem_with_trip_getting)
+            _problemMessage.emit(R.string.problem_with_trip_getting)
         }
     }
 }
